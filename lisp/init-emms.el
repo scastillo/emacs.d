6dcc01ab86da5ea3fe5db580f52f116f8c548a24
scastillo <castillobuiles@gmail.com>
2014-12-03 23:03:08 -0500

    [init-emms] Initial commit

---
+++ lisp/init-emms.el
@@ -0,0 +1,9 @@
+(require 'emms-setup)
+(require 'emms-player-mplayer)
+(emms-standard)
+(emms-default-players)
+(define-emms-simple-player mplayer '(file url)
+  (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma"
+                ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
+                ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a" ".flv" ".ogv" ".pls"))
+  "mplayer" "-slave" "-quiet" "-really-quiet" "-fullscreen")
